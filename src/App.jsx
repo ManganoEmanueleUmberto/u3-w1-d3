5494f9b9111f37b8768fd6f47f7304d01923b99a
Emanuele Umberto Mangano <emanuelemangano15@gmail.com>
2024-04-24 17:43:35 +0200

    search function added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MyNavbar from "./components/MyNavbar";
 import { Container } from "react-bootstrap";
-import Cards from "./components/Cards";
+
 import MyFooter from "./components/MyFooter";
 import Welcome from "./components/Welcome";
 
 import fantasy from "./data/fantasy.json";
 import BookList from "./components/BookList";
-import Search from "./components/Search";
 
 function App() {
   return (
@@ -16,7 +15,6 @@ function App() {
       <MyNavbar />
       <Container>
         <Welcome />
-        <Search />
         <BookList books={fantasy} />
       </Container>
       <MyFooter />
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -6,24 +6,55 @@ class BookList extends Component {
   state = {
     filter: "",
   };
+  handleFieldChange = (propertyValue) => {
+    this.setState({ filter: propertyValue });
+  };
 
   render() {
-    if (this.state.filter) {
-      const filtered = this.props.books.includes(this.state.filter);
+    console.log(this.state.filter);
+    if (this.state.filter !== "") {
       return (
-        <Row className="g-4 mt-4">
-          {filtered.map((books) => {
-            return <SingleBook book={books} />;
-          })}
-        </Row>
+        <>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group className="mb-3" controlId="FormSearch">
+              <Form.Label>Search</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Search a book's name"
+                onChange={(e) => this.handleFieldChange(e.target.value)}
+                value={this.state.filter}
+              />
+            </Form.Group>
+          </Form>
+          <Row className="g-4 mt-4">
+            {this.props.books
+              .filter((book) => book.title.toLocaleLowerCase().includes(this.state.filter.toLocaleLowerCase()))
+              .map((book) => (
+                <SingleBook book={book} />
+              ))}
+          </Row>
+        </>
       );
     } else {
       return (
-        <Row className="g-4 mt-4">
-          {this.props.books.map((books) => {
-            return <SingleBook book={books} />;
-          })}
-        </Row>
+        <>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group className="mb-3" controlId="FormSearch">
+              <Form.Label>Search</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Search a book's name"
+                onChange={(e) => this.handleFieldChange(e.target.value)}
+                value={this.state.filter}
+              />
+            </Form.Group>
+          </Form>
+          <Row className="g-4 mt-4">
+            {this.props.books.map((books) => {
+              return <SingleBook book={books} />;
+            })}
+          </Row>
+        </>
       );
     }
   }
